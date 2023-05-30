56ed71740987561fbcd85963cc7468a8ce31d245
joeyrodo <joeyrodo@gmail.com>
2023-05-30 07:36:44 -0400

    sample seed values

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,19 +1,20 @@
 INSERT INTO department (name)
-VALUES ("Crusaders"),
-       ("Assassins");
+VALUES ("sample_dpmt_1"),
+       ("sample_dpmt_2"),
+       ("sample_dpmt_3");
        
 INSERT INTO role (title, salary, department_id)
 VALUES 
-        ("Commander", 1000000, 1),
-        ("Firefighter", 800000, 1),
-        ("Electrician", 750000, 1),
-        ("Hair Stylist", 450000, 2),
-        ("Motivational Speaker", 100000, 2);
+        ("sample_role_1", 1000000, 1),
+        ("sample_role_2", 800000, 1),
+        ("sample_role_3", 750000, 1),
+        ("sample_role_4", 450000, 2),
+        ("sample_role_5", 100000, 2);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-        ("Kliff", "Undersn", 1, null),
-        ("Sol", "Badguy", 2, 1),
-        ("Ky", "Kiske", 3, 1),
-        ("Millia", "Rage", 4, 2),
-        ("Zato", "One", 5, 2);
+        ("first1", "last1", 1, null),
+        ("first2", "last2", 2, 1),
+        ("first3", "last3", 3, 1),
+        ("first4", "last4", 4, 2),
+        ("first5", "last5", 5, 2);
